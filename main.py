7aca8df3792c111587164a9b117c68b3a7cb80fb
Daniel <kronkowski.daniel@gmail.com>
2023-09-04 20:42:19 +0200

    change server version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from server import Server
 
 HOST = '127.0.0.1'
 PORT = 65432
-INFO = 'version: 0.3.3; creation date: 12.03.2023r'
+INFO = 'version: 0.4.0; creation date: 12.03.2023r'
 server = Server(HOST,PORT,INFO)
 
 server.start()
